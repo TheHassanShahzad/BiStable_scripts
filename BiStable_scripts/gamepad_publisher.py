874155df8be10749306d3b68e0c46536d57e366a
Harris <yuopres@gmail.com>
2024-03-29 12:10:21 +0000

    changed order for button presses to change kp, ki, ... values. mapped joystick inputs to inclination and yaw velocity commands

--- BiStable_scripts/gamepad_publisher.py
+++ BiStable_scripts/gamepad_publisher.py
@@ -24,20 +24,25 @@ class GamepadPublisher(Node):
 
         self.publisher_.publish(msg)
 
-
+def linear_map(input, gain):
+    return input * gain
 
 def main(args=None):
-    # global start_time  # Declare start_time as global
-    # start_time = time.time()  # Initialize start_time
     rclpy.init(args=args)
     node = GamepadPublisher()
     tracking_state = False
-    prev_square_state = None
-    prev_cross_state = None
-    prev_circle_state = None
-    prev_triangle_state = None
+    # prev_square_state = None
+    # prev_cross_state = None
+    # prev_circle_state = None
+    # prev_triangle_state = None
     prev_L1_state = None
     prev_R1_state = None  
+    prev_R2_state = None
+    prev_L2_state = None
+    inclination = 0.0     # Inclination angle in degrees
+    yaw_vel = 0.0         # Yaw velocity in cm/s
+    max_inclination = 8.0 # Maximum inclination angle in degrees
+    max_yaw_vel = 5.0     # Maximum yaw velocity in cm/s
 
     while True:
 
@@ -53,6 +58,10 @@ def main(args=None):
                     left_x = float(joystick['lx'])
                     left_y = float(joystick['ly'])
                     #print(left_x, left_y)
+
+                    # Map joystick values to inclination angle and yaw velocity
+                    inclination = linear_map(left_y, max_inclination)
+                    yaw_vel = linear_map(left_x, max_yaw_vel)
                     
                     # Read button states
                     square_state = joystick['square']
@@ -66,6 +75,7 @@ def main(args=None):
                     R2_state = joystick['r2']
                     #R3_state = joystick['r3']
 
+                    # Press L1 and R1 to toggle tracking
                     if L1_state != prev_L1_state:
                         if R1_state:
                             if tracking_state == False:
@@ -80,99 +90,62 @@ def main(args=None):
                             else:
                                 tracking_state = False
 
-
                     #print(tracking_state)
-                    # Check if button is pressed down
-                    if square_state and not prev_square_state:
-                        if L1_state:
-                            #print("kp -0.1")
+                                
+                    # L1 to change value by -0.1
+                    if L1_state and not prev_L1_state:
+                        if square_state:
                             kp_change = -0.1
-                        
-                    if cross_state and not prev_cross_state:
-                        if L1_state:
-                            #print("ki -0.1")
+                        if cross_state:
                             ki_change = -0.1
-
-                    if circle_state and not prev_circle_state:
-                        if L1_state:
-                            #print("kd -0.1")
+                        if circle_state:
                             kd_change = -0.1
-
-                    if triangle_state and not prev_triangle_state:
-                        if L1_state:
-                            #print("eq -0.1")
+                        if triangle_state:
                             eq_change = -0.1
 
-                    if square_state and not prev_square_state:
-                        if L2_state:
-                            #print("kp -0.01")
+                    # L2 to change value by -0.01
+                    if L2_state and not prev_L2_state:
+                        if square_state:
                             kp_change = -0.01
-                        
-                    if cross_state and not prev_cross_state:
-                        if L2_state:
-                            #print("ki -0.01")
+                        if cross_state:
                             ki_change = -0.01
-
-                    if circle_state and not prev_circle_state:
-                        if L2_state:
-                            #print("kd -0.01")
+                        if circle_state:
                             kd_change = -0.01
+                        if triangle_state:
+                            eq_change = -0.01    
 
-                    if triangle_state and not prev_triangle_state:
-                        if L2_state:
-                            #print("eq -0.01")
-                            eq_change = -0.01
-
-                    if square_state and not prev_square_state:
-                        if R1_state:
-                            #print("kp +0.01")
+                    # R1 to change value by +0.1
+                    if R1_state and not prev_R1_state:
+                        if square_state:
                             kp_change = 0.1
-                        
-                    if cross_state and not prev_cross_state:
-                        if R1_state:
-                            #print("ki +0.01")
+                        if cross_state:
                             ki_change = 0.1
-
-                    if circle_state and not prev_circle_state:
-                        if R1_state:
-                            #print("kd +0.01")
+                        if circle_state:
                             kd_change = 0.1
+                        if triangle_state:
+                            eq_change = 0.1 
 
-                    if triangle_state and not prev_triangle_state:
-                        if R1_state:
-                            #print("eq +0.01")
-                            eq_change = 0.1
-
-                    if square_state and not prev_square_state:
-                        if R2_state:
-                            #print("kp +0.1")
+                    # R2 to change value by +0.01
+                    if R2_state and not prev_R2_state:
+                        if square_state:
                             kp_change = 0.01
-                        
-                    if cross_state and not prev_cross_state:
-                        if R2_state:
-                            #print("ki +0.1")
+                        if cross_state:
                             ki_change = 0.01
-
-                    if circle_state and not prev_circle_state:
-                        if R2_state:
-                            #print("kd +0.1")
+                        if circle_state:
                             kd_change = 0.01
-
-                    if triangle_state and not prev_triangle_state:
-                        if R2_state:
-                            #print("eq +0.1")
+                        if triangle_state:
                             eq_change = 0.01
 
                     # Update previous button states
-                    prev_square_state = square_state
-                    prev_cross_state = cross_state
-                    prev_circle_state = circle_state
-                    prev_triangle_state = triangle_state
+                    # prev_square_state = square_state
+                    # prev_cross_state = cross_state
+                    # prev_circle_state = circle_state
+                    # prev_triangle_state = triangle_state
                     prev_L1_state = L1_state
                     prev_R1_state = R1_state
 
-                    node.publish_data(left_y, left_x, eq_change, kp_change, ki_change, kd_change, tracking_state)
-                    # data_to_be_published = [left_x, left_y, eq_change, kp_change, ki_change, kd_change, tracking_state]
+                    node.publish_data(inclination, yaw_vel, eq_change, kp_change, ki_change, kd_change, tracking_state)
+                    # data_to_be_published = [inclination, yaw_vel, eq_change, kp_change, ki_change, kd_change, tracking_state]
                     # print(data_to_be_published)
                     sleep(0.05)
 
